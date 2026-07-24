31822cbf995074cf5eb454714b33dca874aca50e
lish99 <lish99@users.noreply.github.com>
2026-07-24 06:16:16 +0000

    deploy: 31fdacdee96a2a9da0e5ac89caf2502a74be133c

---
+++ js/chunk-dhCGdq_m.js
@@ -0,0 +1,1 @@
+import{a as r}from"./chunk-CdpzrYGk.js";import{o,Q as s,P as t}from"./chunk-C5IKhFkM.js";const n=r({},[["render",function(r,n){const a=t("router-view");return o(),s(a)}]]);export{n as default};
